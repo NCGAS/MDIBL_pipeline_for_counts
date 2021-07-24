428f770d2f7f42cc5ba599d8ea903b385cca9166
ssander5 <ss93@iu.edu>
2021-07-23 23:35:01 -0400

    Update pull.sh

--- software/hisat2/pull.sh
+++ software/hisat2/pull.sh
@@ -5,4 +5,4 @@ if [ ! $(command -v singularity) ]; then
 fi
 
 singularity pull https://depot.galaxyproject.org/singularity/hisat2:2.2.1--h1b792b2_3
-mv -v hisat* singularity-hisat2-2.2.1.sif
+mv -v hisat2:2.2* singularity-hisat2-2.2.1.sif
